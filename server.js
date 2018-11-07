2a19cdd4d94360ef52b429a44c963dc7fe2b3c59
Lada Kesseler <Lada.Kesseler1@T-Mobile.com>
2018-11-06 16:18:02 -0800

    Setup start sript and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs'); // handlebars framework
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
@@ -77,6 +79,6 @@ app.get('/maintenance', (request, response) => {
         });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
